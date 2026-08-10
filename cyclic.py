5d0f0fded93e802de77aaa61e752538f9d23e8a0
assklahoma <fghjnzv1299@gmail.com>
2026-08-10 16:02:05 +0300

    Add cyclic structure for summing multiples of 3
    Implement cyclic structure to calculate sum of multiples of 3 up to a two-digit number.

---
+++ cyclic.py
@@ -0,0 +1,17 @@
+print("Practical Work #1. Task 2: Cyclic Structures")
+
+while True:
+    n = int(input("Enter a two-digit number (N): "))
+    if 10 <= abs(n) <= 99:
+        break
+    print("Invalid input. Please enter a valid two-digit number.")
+
+i = 1
+total_sum = 0
+
+while i <= n:
+    if i % 3 == 0:
+        total_sum += i
+    i += 1
+
+print(f"The final sum of the series (S) = {total_sum}")
